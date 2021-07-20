25c4fde30ab3564a8dff5c21bc56c9c05e111f33
eduardo <eduardo.vinoles@tarmac.io>
2021-07-20 12:40:22 -0300

    add icon to title

--- piza.js
+++ piza.js
@@ -293,6 +293,8 @@ window.onload = function () {
 
 
 }
+
+
 const addToForm = function (event) {
     let lista = this.parentNode.getElementsByClassName("cartElement")
     // console.log(lista)
@@ -301,6 +303,8 @@ const addToForm = function (event) {
     var theUrl = "http://localhost:3000";
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            let toast = document.querySelector(".toast")
+           toast.show()
             console.log(xmlhttp.response);
         }
     }
